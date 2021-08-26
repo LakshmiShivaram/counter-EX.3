048b49fdff725655715c98b6a2f9a388dbe7e82e
LakshmiShivaram <lakshmi.shivaram@accenture.com>
2021-08-26 21:03:30 +0530

    1 file modified

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, map, skip, startWith, take } from 'rxjs/operators';
 import { increment, decrement, reset } from '../counter.actions';
 
 @Component({
@@ -11,9 +11,38 @@ import { increment, decrement, reset } from '../counter.actions';
 })
 export class MyCounterComponent {
   count$: Observable<number>;
+  operations$: BehaviorSubject<string>;
 
   constructor(private store: Store<{ count: number }>) {
     this.count$ = store.select('count');
+
+    // square ap emiited
+    this.count$
+      .pipe(map(res => res * res))
+      .subscribe(value => console.log('map', value));
+
+    //first 3 values
+    this.count$.pipe(take(3)).subscribe(value => console.log('Take', value));
+
+    // filteer for even
+    this.count$
+      .pipe(filter(result => result % 2 === 0))
+      .subscribe(value => console.log('even values', value));
+
+    //skip
+    this.count$
+      .pipe(skip(3))
+      .subscribe(value => console.log(' fisrt 3 values skipped', value));
+
+    //start with operator
+    this.count$
+      .pipe(startWith(100))
+      .subscribe(value => console.log(' startwith', value));
+
+    // 0*(1,2,3,),
+    // 1*(1,2,3,),
+    // 2*(1,2,3),
+    // 4*(1,2,3)
   }
 
   increment() {
